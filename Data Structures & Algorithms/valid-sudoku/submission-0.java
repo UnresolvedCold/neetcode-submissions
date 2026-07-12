5084906db759e7a3c4e3863a63feab32d7fc0f70
Shubham Kumar <unresolved.shubham@gmail.com>
2026-07-12 12:15:00 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,57 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        return checkHorizontal(board) && checkVertical(board) && checkCells(board);
+    }
+
+    private boolean checkHorizontal(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            Set<Character> hset = new HashSet<Character>();
+
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                if (hset.contains(board[i][j])) return false;
+                hset.add(board[i][j]);
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkVertical(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            Set<Character> hset = new HashSet<Character>();
+
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] == '.') continue;
+                if (hset.contains(board[j][i])) return false;
+                hset.add(board[j][i]);
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkCells(char[][] board) {
+        for (int i = 0; i < 9; i+=3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!checkBox(board, i, j)) return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkBox(char[][] board, int a, int b) {
+        Set<Character> hset = new HashSet<Character>();
+
+        for (int i=a; i<a+3; i++) {
+            for (int j=b; j<b+3; j++) {
+                if (board[i][j] == '.') continue;
+                if (hset.contains(board[i][j])) return false;
+                hset.add(board[i][j]);
+            }
+        }
+
+        return true;
+    }
+}
